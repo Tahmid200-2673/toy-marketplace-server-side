d7b5945a99ad01463fb03823d01dbd0cbd079510
Tahmid200-2673 <tahmidahmed22673@gmail.com>
2023-05-21 04:07:34 +0600

    bonus section bacckend done

--- index.js
+++ index.js
@@ -34,36 +34,78 @@ async function run() {
 
     const toysCollection = client.db('toyStore').collection('toys');
 
+   
+
+    // app.get('/toys', async (req, res) => {
+    //     console.log(req.query.email);
+    //     let query = {};
+    //     if (req.query?.email) {
+    //         query = { email: req.query.email }
+    //     }
+    //     const result = await toysCollection.find(query).toArray();
+    //     res.send(result);
+    // }) 
+
+    app.get('/toys', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      
+      const sortField = req.query?.sortField || 'price'; // Default sort field is 'price'
+      const sortOrder = req.query?.sortOrder === 'desc' ? -1 : 1; // Default sort order is ascending
+    
+      const result = await toysCollection.find(query).sort({ [sortField]: sortOrder }).toArray();
+      res.send(result);
+    });
+    
     
 
     app.get('/toys', async (req, res) => {
-        console.log(req.query.email);
-        let query = {};
-        if (req.query?.email) {
-            query = { email: req.query.email }
-        }
-        const result = await toysCollection.find(query).toArray();
-        res.send(result);
-    }) 
+      console.log(req.query);
+      const initial = parseInt(req.query.page) || 0;
+      const end = parseInt(req.query.limit) || 20;
+      const proceed = initial * end;
+      //  const cursor = toysCollection.find();
+      const result = await toysCollection.find().skip(proceed).limit(end).toArray();
+     res.send(result);
+  })
 
-    app.get('/toys', async (req, res) => {
-        const cursor = toysCollection.find();
-        const result = await cursor.toArray();
-       res.send(result);
-    })
+    
+
+    //  app.get('/toys/:id', async (req, res) => {
+    //      const id = req.params.id;
+    //     const query = { _id: new ObjectId(id) }
+    
+        
+    
+    //     const result = await toysCollection.findOne(query, options);
+    //      res.send(result);
+    //  })
 
     app.get('/toys/:id', async (req, res) => {
+      try {
         const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-    
+        const query = { _id: new ObjectId(id) };
+
         const options = {
           // Include only the `title` and `imdb` fields in the returned document
-          projection: { title: 1, price: 1, service_id: 1, img: 1 },
-       };
+          projection: { name: 1, sellerName:1 , 
+            email:1 , subcategory:1 , rating:1 , description:1 , price: 1, quantity: 1, pictureUrl: 1 },
+      };
+        const result = await toysCollection.findOne(query , options);
+        
+        if (!result) {
+          return res.status(404).send('Toy not found');
+        }
     
-        const result = await toysCollection.findOne(query, options);
         res.send(result);
-    })
+      } catch (error) {
+        console.error('Error retrieving toy:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
     
     app.post('/toys', async (req, res) => {
         const toy = req.body;
@@ -71,6 +113,34 @@ async function run() {
         const result = await toysCollection.insertOne(toy);
         res.send(result);
     });
+
+    app.put('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedtoy = req.body;
+
+      const toy = {
+          $set: {
+            
+             price: updatedtoy.price,
+              quantity: updatedtoy.quantity,
+              description: updatedtoy.description,
+          }
+      }
+
+      const result = await toysCollection.updateOne(filter, toy, options);
+      res.send(result);
+  })
+
+    
+
+    app.delete('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+  })
 
   
 
